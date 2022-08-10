a8630e9887cb1ee22e3a8b59d00dbb59e3c873e1
Marioara Pavelea <Pavelea.Gr.Marioara@student.utcluj.ro>
2022-08-10 14:15:34 +0300

    save the values from contact us input field as an object in local storage

--- contactinfo.js
+++ contactinfo.js
@@ -1,25 +1,36 @@
 const inputMessage = document.getElementById("message");
-const inputSubj = document.getElementById("subj");
+const inpFName = document.getElementById("fname");
+const inpLName = document.getElementById("lname");
+const inpEmail = document.getElementById("email");
+const inpPhone = document.getElementById("phone");
+
 const submitBtn = document.getElementById("submit-contact");
 
 function loadFunction() {
-  const k = localStorage.length - 1;
-  const keyL = localStorage.key(k);
-  const storedValue = localStorage.getItem(keyL);
+  const storedValue = JSON.parse(localStorage.getItem("inputVal"));
   console.log(storedValue);
   if (storedValue) {
-    inputMessage.value = storedValue;
+    inputMessage.value = storedValue.message;
+    inpFName.value = storedValue.firstname;
+    inpLName.value = storedValue.lastname;
+    inpEmail.value = storedValue.email;
+    inpPhone.value = storedValue.phone;
   } else {
     inputMessage.value = "";
   }
 }
 
 submitBtn.addEventListener("click", function () {
-  const key = inputSubj.value;
-  const value = inputMessage.value;
-
-  console.log(key);
-  console.log(value);
+  const key = "inputVal";
+  let arr = {
+    message: inputMessage.value,
+    firstname: inpFName.value,
+    lastname: inpLName.value,
+    email: inpEmail.value,
+    phone: inpPhone.value,
+  };
+  const value = JSON.stringify(arr);
+  console.log(value, key);
 
   if (key && value) {
     localStorage.setItem(key, value);
